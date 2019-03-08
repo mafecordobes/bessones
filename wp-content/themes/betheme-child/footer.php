8a25a46c2a94480f7f39e6420d1780adbc5d4dc9
Mafe Cordobes <mfcj307@gmail.com>
2019-03-07 19:32:36 -0500

    Homepage

---
+++ wp-content/themes/betheme-child/footer.php
@@ -0,0 +1,31 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-footer-20 text-center">
+				<img src="<?php echo home_url(); ?>/wp-content/uploads/bessones/bessones_logo.png" class="img-responsive">
+				<div class="container-icon">
+					<a><i class="icon-facebook"></i></a>
+					<a><i class="icon-twitter"></i></a>
+					<a><i class="icon-instagram"></i></a>
+				</div>
+			</div>
+			<div class="col-footer-20">
+				<h5>Bessones Turismo</h5>
+				<p>Registro Nacional de Turismo Nro. 34758</p>
+				<p>Turismo, alojamiento, tours, eventos, transporte, playas privadas todo en santa marta y la costa.</p>
+			</div>
+			<div class="col-footer-20">
+				<h5>Información de contacto</h5>
+				<p><span class="icon-location-line"></span>Calle 138 # 5-11 Sierra Grata <br/>Bello horizonte</p>
+				<p><a><span class="icon-call"></span>5-4218 123</a></p>
+				<p><span class="icon-mobile"></span>317 241 4460</p>
+			</div>
+			<div class="col-footer-40 text-center">
+				<div>
+					<img src="<?php echo home_url(); ?>/wp-content/uploads/2019/03/dc30d609b3c51586675d70adac803d02.jpg" class="ojos-footer">
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+<?php wp_footer(); ?>
