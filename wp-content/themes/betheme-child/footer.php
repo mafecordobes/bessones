c5e913b939de527df0971ce88c91b21aa6ce05d1
Mafe Cordobes <mfcj307@gmail.com>
2019-03-08 14:55:00 -0500

    Home mobile

--- wp-content/themes/betheme-child/footer.php
+++ wp-content/themes/betheme-child/footer.php
@@ -4,21 +4,23 @@
 			<div class="col-footer-20 text-center">
 				<img src="<?php echo home_url(); ?>/wp-content/uploads/bessones/bessones_logo.png" class="img-responsive">
 				<div class="container-icon">
-					<a><i class="icon-facebook"></i></a>
-					<a><i class="icon-twitter"></i></a>
-					<a><i class="icon-instagram"></i></a>
+					<a href="https://www.facebook.com/bessonesturismoeventos" target="_blank"><i class="icon-facebook"></i></a>
+					<a href="https://twitter.com/rangel_stella" target="_blank"><i class="icon-twitter"></i></a>
+					<a href="https://www.instagram.com/bessonesturismocolombia/" target="_blank"><i class="icon-instagram"></i></a>
 				</div>
 			</div>
 			<div class="col-footer-20">
 				<h5>Bessones Turismo</h5>
 				<p>Registro Nacional de Turismo Nro. 34758</p>
 				<p>Turismo, alojamiento, tours, eventos, transporte, playas privadas todo en santa marta y la costa.</p>
+				<p><img src="<?php echo home_url(); ?>/wp-content/uploads/2019/03/pet.png" class="pet-friendly">Somos pet friendly</p>
+
 			</div>
 			<div class="col-footer-20">
 				<h5>Información de contacto</h5>
 				<p><span class="icon-location-line"></span>Calle 138 # 5-11 Sierra Grata <br/>Bello horizonte</p>
-				<p><a><span class="icon-call"></span>5-4218 123</a></p>
-				<p><span class="icon-mobile"></span>317 241 4460</p>
+				<p><a href="tel:+5754218123"><span class="icon-call"></span>5-4218 123</a></p>
+				<p><a href="whatsapp://send?abid=+573172414460"><span class="icon-mobile"></span>317 241 4460</a></p>
 			</div>
 			<div class="col-footer-40 text-center">
 				<div>
